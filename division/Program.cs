2446ad4bb96696d0a68515fcffeaf62395cb352f
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-01 15:55:50 -0600

    Add project files.

---
+++ division/Program.cs
@@ -0,0 +1,18 @@
+﻿float num1, num2;
+float division;
+
+Console.Write("Digite un núnmero: ");
+num1 = float.Parse(Console.ReadLine());
+
+Console.Write("Digite otro número: ");
+num2 = float.Parse(Console.ReadLine());
+
+try
+{
+    division = num1 / num2;
+    Console.WriteLine($"{num1} / {num2} = {division}");
+}
+catch(DivideByZeroException)
+{
+    Console.WriteLine("No se puede dividir entre 0");
+}
---
+++ ejercicio1GastosGira/Program.cs
@@ -0,0 +1,28 @@
+﻿// Un grupo de estudiantes de la UAM viaja a León para una visita técnica. Deben repartir equitativamente los costos (combustible, peajes, refrigerios)
+//Si nadie confirmó asistencia (0 participantes), el cálculo por persona falla.
+
+//Solicite: costo total (C$) y número de participantes.
+decimal costoTotal, costoPorPersona = 0;
+int numeroParticipantes = 0;
+
+
+Console.WriteLine("Vamos a calcular el costo por persona de la visita técnica.");
+Console.Write("Ingrese el costo total: ");
+costoTotal = Convert.ToDecimal(Console.ReadLine());
+
+while (true)
+{
+    try
+    {
+        Console.Write("Ingrese el número de participantes: ");
+        numeroParticipantes = Convert.ToInt32(Console.ReadLine());
+        costoPorPersona = costoTotal / numeroParticipantes;
+        break;
+    }
+    catch (DivideByZeroException)
+    {
+        Console.WriteLine("No puede dividir entre cero. Regrese a primaria.");
+    }
+}
+
+Console.WriteLine($"El costo por participante es {costoPorPersona}");

6dcf474f7f34a598b2228607792fe206fca17e57
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-01 19:06:06 -0600

    Add ejercicio3 (registro de cliente)

---
+++ ejercicio3RegistroCliente/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("REGISTRO DE CLIENTE");
+
+string nombreCliente = "";
+string numeroWhatsapp = "";
+
+while (true)
+{
+    try
+    {
+        Console.Write("Ingrese su nombre completo: ");
+        nombreCliente = Console.ReadLine();
+
+        Console.WriteLine($"nombreCliente.Length = {nombreCliente.Length}");
+        nombreCliente = (nombreCliente.Length == 0) ? null: nombreCliente.Trim().ToUpper();
+
+        if (nombreCliente == null)
+        {
+            throw new NullReferenceException();
+        }
+        break;
+    }
+    catch (NullReferenceException)
+    {
+        Console.WriteLine("El nombre no puede estar vacío");
+    }
+}
+
+while (true)
+{
+    try
+    {
+        Console.Write("Ingrese su número de whatsapp, sin guiones ni código de país: ");
+        numeroWhatsapp = Console.ReadLine();
+
+        if (numeroWhatsapp.Length != 8)
+        {
+            throw new FormatException();
+        }
+        break;
+    }
+    catch (FormatException)
+    {
+        Console.WriteLine("Ingrese un número válido, con 8 dígitos.");
+    }
+}
+
+Console.WriteLine($"Nombre: {nombreCliente}");
+Console.WriteLine($"Número de WhatsApp: {numeroWhatsapp}");

f65943ed18dd44bd9b7fa472581083df7e6e2d7c
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-07 09:55:43 -0600

    Add ejercicio1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i < 11; i++)
+            {
+                Console.WriteLine($"{i}");
+            }
+        }
+    }
+}

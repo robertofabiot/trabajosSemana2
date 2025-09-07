4c34e59212e1e5f8f997f1aee9e5cdb88a82d6f5
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-07 10:00:41 -0600

    Add ejercicio2

---
+++ ejercicio2/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, suma = 0;
+            Console.Write("Ingrese un número entero positivo: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            int i = 1;
+            while (i <= numero)
+            {
+                suma += i;
+                i++;
+            }
+            Console.WriteLine($"La suma desde 1 hasta {numero} es {suma}");
+        }
+    }
+}

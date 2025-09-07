03b40f09c56007710a6e513cfc8a3b15c70501b6
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-07 10:30:20 -0600

    Add ejercicio5

--- practica/ejercicio5/Program.cs
+++ practica/ejercicio5/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int numeroActual = 0;
+            int[] numeros = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese un número entero: ");
+                numeroActual = Convert.ToInt32(Console.ReadLine());
+                numeros[i] = numeroActual;
+            }
+
+            int pares = 0; int impares = 0;
+            for(int i = 0; i < 10; i++)
+            {
+                if (numeros[i] % 2 == 0)
+                    pares++;
+                else
+                    impares++;
+            }
+            Console.WriteLine($"La cantidad de números pares fue de: {pares}");
+            Console.WriteLine($"La cantidad de números impares fue de: {impares}");
         }
     }
 }

da7e18eecd5d596cf46af3aab7277e5aff496d11
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-07 10:14:38 -0600

    Add ejercicio4

---
+++ ejercicio4/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace ejercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0; int i = 1;
+            Console.Write("Ingrese un número: ");
+            numero = Convert.ToInt32(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine($"{numero} * {i} = {numero * i}");
+                i++;
+            } while (i <= 10);
+        }
+    }
+}

aaaab96ef4e96761862a10de6c25a235c2d3aa09
robertofabiot <rftercerom@uamv.edu.ni>
2025-09-07 11:22:11 -0600

    Ad ejercicio10

---
+++ practica/ejercicio10/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ejercicio10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese el número de días: "); int dias = Convert.ToInt32(Console.ReadLine()); Console.WriteLine($"{dias / 7} semanas y {dias % 7} días");
+        }
+    }
+}
